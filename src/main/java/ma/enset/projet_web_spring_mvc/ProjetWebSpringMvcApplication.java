de880a6cbca8cadd7546a3bbbb333840b772c1f3
abdelwafi-laaouissi <laaouissi.f@gmail.com>
2025-05-22 21:26:31 +0100

    Initialisation du projet Gestion Produit

---
+++ src/main/java/ma/enset/projet_web_spring_mvc/ProjetWebSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_web_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetWebSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/projet_web_spring_mvc/ProjetWebSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_web_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
